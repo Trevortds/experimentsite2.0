1af542c1aa3b4686d7e09c32f00542e8c3b91d4f
TrevorTDS <trevortds3@gmail.com>
2016-10-16 14:08:47 -0700

    fixed syntax error

--- experimentsite/experimentsite/urls.py
+++ experimentsite/experimentsite/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
 
     url(r'^admin/', admin.site.urls),
     url(r'', include('log.urls')),
-    url(r'^login/$', views.login, {'template_name': 'login.html'},
+    url(r'^login/$', views.login, {'template_name': 'login.html'}),
 ]
